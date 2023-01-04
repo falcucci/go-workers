6c8b54ffeb582d8c045de0b80fb4b0d91ecda4b5
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-01-04 17:38:36 +0100

    add people table
    this table will be used to store people's names and surnames.

--- migrations/migrations/20230104173616_people.js
+++ migrations/migrations/20230104173616_people.js
@@ -1,8 +1,18 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("people", function (table) {
+    table.increments("id").primary();
+    table.string("name", 255);
+    table.string("surname", 255);
 
-exports.up = function(knex, Promise) {
-  
+    table.comment("Table from people");
+    table.index(
+      ["id"],
+      "idx_people_1"
+    );
+    table.engine("InnoDB");
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("people");
 };
