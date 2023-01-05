314273260ba55b28eefeb78e60aa86624eadf85b
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-01-05 16:12:03 +0100

    fix typo in init.sh
    the typo was causing the script to fail.

--- scripts/db/init.sh
+++ scripts/db/init.sh
@@ -4,6 +4,6 @@ export PGPASSWORD='localhost';
 psql -v ON_ERROR_STOP=1 --username "people" --dbname "people_db" <<-EOSQL
   CREATE USER people WITH PASSWORD 'localhost';
   CREATE DATABASE people_db;
-  GRANT ALL PRIVILEGES ON DATABASE people_db TO races;
-  \connect people_db races
+  GRANT ALL PRIVILEGES ON DATABASE people_db TO people;
+  \connect people_db people
 EOSQL
